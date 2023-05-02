e72ab58aac4000ac412f7ece1c11de15c84a6b36
MacBook <mac@MacBook-Pro-MacBook.local>
2023-05-02 23:27:34 +0300

    den

--- temporary_file.py
+++ temporary_file.py
@@ -1,4 +1,3 @@
 dict_1 = dict(age=input('vozrast?'), name=input('imya?'), sname=input('familiya?'), work=input('rabota?'))
 
-
-print(dict_1.get('work'),'\n', '-' * 10,'\n', dict_1.get('sname'), dict_1.get('name'), ',', dict_1.get('age'))
+print(dict_1.get('work'),'\n', '-' * 10,'\n', dict_1.get('sname'), dict_1.get('name'), ', ', dict_1.get('age'), sep='')
