1d405643d27a3ec59ae3f331e5dbd740dbe36a60
MacBook <mac@MacBook-Pro-MacBook.local>
2023-05-03 19:23:20 +0300

    den

--- temporary_file.py
+++ temporary_file.py
@@ -1,10 +1,12 @@
-dict_1 = dict(age=input('vozrast?'), name=input('imya?'), sname=input('familiya?'), work=input('rabota?'))
+user_1 = dict(age=input('vozrast?'), name=input('imya?'), sname=input('familiya?'), work=input('rabota?'))
 user_2 = dict(age='28', name='Andrey', sname='Petrov', work='ment')
 
-print(dict_1.get('work'),'\n', '-' * 10,'\n', dict_1.get('sname'), dict_1.get('name'), ', ', dict_1.get('age'), sep='')
+print(user_1.get('work'), '\n', '-' * 10, '\n', user_1.get('sname'), user_1.get('name'), ', ', user_1.get('age'),
+      sep='')
 
 a = int(user_2["age"])
-b = int(dict_1["age"])
+b = int(user_1["age"])
+
 avg = int((a + b) / 2)
 
 print(avg)
