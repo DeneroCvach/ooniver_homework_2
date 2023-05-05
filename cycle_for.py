cec7f329426b86eaf86f53047e9e3be77e25931d
MacBook <mac@MacBook-Pro-MacBook.local>
2023-05-05 19:34:02 +0300

    den

--- cycle_for.py
+++
@@ -1,4 +0,0 @@
-number_list = [0, 1, 2, 3, 4, 5]
-
-for i in number_list:
-    print(i, end=" ")
--- many_odds.py
+++
@@ -1,9 +0,0 @@
-# for i in range(1, 100, 2):
-#     print(i)
-i = 1
-while i < 101:
-     if i % 2:
-         print('odd', i)
-     else:
-         print('even', i)
-     i += 1
